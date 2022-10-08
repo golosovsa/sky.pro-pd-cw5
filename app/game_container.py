8b3a9b0387a9f850274d955d16d167ffe364391e
grm <golosov_sa@mail.ru>
2022-10-09 00:17:25 +0300

    replaced match-case statement

--- app/game_container.py
+++ app/game_container.py
@@ -111,15 +111,24 @@ class Game:
         if self._current_screen_index != 3 or self._arena is None:
             return
 
-        match action:
-            case "hit":
-                self._arena.make_a_player_hit()
-            case "skill":
-                self._arena.use_player_skill()
-            case "skip":
-                self._arena.skip_turn()
-            case _:
-                return
+        if action == "hit":
+            self._arena.make_a_player_hit()
+        elif action == "skill":
+            self._arena.use_player_skill()
+        elif action == "skip":
+            self._arena.skip_turn()
+        else:
+            return
+
+        # match action:
+        #     case "hit":
+        #         self._arena.make_a_player_hit()
+        #     case "skill":
+        #         self._arena.use_player_skill()
+        #     case "skip":
+        #         self._arena.skip_turn()
+        #     case _:
+        #         return
 
         if self._arena.is_game_end:
             self._end_game()
